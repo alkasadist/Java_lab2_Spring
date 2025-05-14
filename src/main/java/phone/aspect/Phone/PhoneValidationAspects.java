098ef976b5ed190dcfa2ff98fcc44c3c68590a40
alkasadist <alkasadist@gmail.com>
2025-05-14 20:35:46 +0300

    Added validation aspects to Phone.answer() && Phone.drop()

--- src/main/java/phone/aspect/Phone/PhoneValidationAspects.java
+++ src/main/java/phone/aspect/Phone/PhoneValidationAspects.java
@@ -12,6 +12,7 @@ import phone.State;
 @Aspect
 @Component
 public class PhoneValidationAspects {
+
     @Around("execution(* phone.Phone.call(String))")
     public Object validateCall(ProceedingJoinPoint joinPoint) throws Throwable {
         String toNumber = (String) joinPoint.getArgs()[0];
@@ -25,18 +26,43 @@ public class PhoneValidationAspects {
             return false;
         }
         if (fromPhone.getState() == State.BLOCKED) {
-            System.out.println("ERROR: You are blocked: your balance is " + fromPhone.getBalance());
+            System.out.println("ERROR: you are blocked: your balance is " + fromPhone.getBalance());
             return false;
         }
         if (toPhone.getNumber().equals(fromPhone.getNumber())) {
-            System.out.println("ERROR: You cannot call yourself.");
+            System.out.println("ERROR: you cannot call yourself.");
             return false;
         }
         if (fromPhone.getState() == State.CALLING) {
-            System.out.println("ERROR: You are already calling someone.");
+            System.out.println("ERROR: you are already calling someone.");
             return false;
         }
 
         return joinPoint.proceed();
     }
+
+    @Around("execution(* phone.Phone.answer())")
+    public Object validateAnswer(ProceedingJoinPoint joinPoint) throws Throwable {
+        Phone fromPhone = (Phone) joinPoint.getTarget();
+
+        if (fromPhone.getState() != State.RINGING) {
+            System.out.println("ERROR: nobody is calling you.");
+            return false;
+        }
+
+        return joinPoint.proceed();
+    }
+
+    @Around("execution(* phone.Phone.drop())")
+    public Object validateDrop(ProceedingJoinPoint joinPoint) throws Throwable {
+        Phone fromPhone = (Phone) joinPoint.getTarget();
+
+        if (fromPhone.getState() != State.IN_CALL) {
+            System.out.println("ERROR: you are not in the call.");
+            return false;
+        }
+
+        return joinPoint.proceed();
+    }
+
 }
