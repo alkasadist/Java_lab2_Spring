4cd133b4615ea77493ce7e19b845a1068526d24a
alkasadist <alkasadist@gmail.com>
2025-05-16 17:18:04 +0300

    log file autocreation added

--- src/main/java/phone/aspect/common/BeanMethodLogger.java
+++ src/main/java/phone/aspect/common/BeanMethodLogger.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Component;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalTime;
 
 @Aspect
@@ -14,13 +17,34 @@ import java.time.LocalTime;
 public class BeanMethodLogger {
     private final String logFileName = System.getProperty("user.dir") + "/logs/logs.log";
 
+    public BeanMethodLogger() {
+        ensureLogFileExists();
+    }
+
+    private void ensureLogFileExists() {
+        try {
+            Path logPath = Paths.get(logFileName);
+            Path logDir = logPath.getParent();
+
+            if (!Files.exists(logDir)) {
+                Files.createDirectories(logDir);
+            }
+            if (!Files.exists(logPath)) {
+                Files.createFile(logPath);
+            }
+
+        } catch (IOException e) {
+            System.err.println("LOGGING ERROR: couldn't initialize log file: " + e.getMessage());
+        }
+    }
+
     @Pointcut("execution(* phone..*(..))")
     public void allPhoneBeans() {}
 
     @Before("allPhoneBeans()")
     public void logBeforeMethod(JoinPoint joinPoint) {
         String logText = "[CALLED AT]    " + LocalTime.now() + " [METHOD] "
-                            + joinPoint.getSignature() + "\n";
+                + joinPoint.getSignature() + "\n";
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(logFileName, true))) {
             writer.write(logText);
@@ -32,7 +56,7 @@ public class BeanMethodLogger {
     @AfterReturning(pointcut = "allPhoneBeans()", returning = "result")
     public void logAfterMethod(JoinPoint joinPoint, Object result) {
         String logText = "[RETURNED AT]  " + LocalTime.now() + " [METHOD] "
-                            + joinPoint.getSignature() + "  ->  " + result + "\n";
+                + joinPoint.getSignature() + "  ->  " + result + "\n";
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(logFileName, true))) {
             writer.write(logText);
